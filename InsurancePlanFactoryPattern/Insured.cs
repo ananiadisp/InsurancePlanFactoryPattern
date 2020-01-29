aa9345d70f00ade6c873c798fd6e7a651eb90ba4
ananiadisp <ananiadis_platonas@hotmail.com>
2020-01-29 13:05:31 +0200

    Initialization of "Product" class Insured

---
+++ InsurancePlanFactoryPattern/Insured.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsurancePlanFactoryPattern
+{
+    abstract class Insured
+    {
+        public abstract int InsuredAge { get; }
+        public abstract string InsurancePlan { get; }
+        public abstract decimal InsurancePlanPrice { get; set; }
+        public abstract int Appointments { get; set; }
+    }
+}
